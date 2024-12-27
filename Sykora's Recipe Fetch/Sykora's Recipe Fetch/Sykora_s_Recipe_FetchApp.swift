675d216275b024a19fe02e828e58de459170ae25
Brenden Sykora <10047807+sykorabw@users.noreply.github.com>
2024-12-27 11:56:13 -0600

    base project

---
+++ Sykora's Recipe Fetch/Sykora's Recipe Fetch/Sykora_s_Recipe_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sykora_s_Recipe_FetchApp.swift
+//  Sykora's Recipe Fetch
+//
+//  Created by Brenden Sykora on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Sykora_s_Recipe_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sykora's Recipe Fetch/Sykora's Recipe FetchTests/Sykora_s_Recipe_FetchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sykora_s_Recipe_FetchTests.swift
+//  Sykora's Recipe FetchTests
+//
+//  Created by Brenden Sykora on 12/24/24.
+//
+
+import Testing
+@testable import Sykora_s_Recipe_Fetch
+
+struct Sykora_s_Recipe_FetchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
